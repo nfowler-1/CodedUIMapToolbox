8b5d44b973e2b6c5957c32fe5f21a9d8aa5da6f0
nfowler-1 <nfowler@asurint.com>
2017-04-14 19:21:54 -0400

    VS 2013 UIMap Toolbox Version

---
+++ VS2013/Source/UIMapToolbox.UI/FindElementForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace UIMapToolbox.UI
+{
+    partial class FindElementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFindWhat = new System.Windows.Forms.TextBox();
+            this.btnFindNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fi&nd what:";
+            // 
+            // txtFindWhat
+            // 
+            this.txtFindWhat.Location = new System.Drawing.Point(76, 10);
+            this.txtFindWhat.Name = "txtFindWhat";
+            this.txtFindWhat.Size = new System.Drawing.Size(137, 21);
+            this.txtFindWhat.TabIndex = 1;
+            // 
+            // btnFindNext
+            // 
+            this.btnFindNext.Location = new System.Drawing.Point(140, 37);
+            this.btnFindNext.Name = "btnFindNext";
+            this.btnFindNext.Size = new System.Drawing.Size(75, 23);
+            this.btnFindNext.TabIndex = 2;
+            this.btnFindNext.Text = "&Find Next";
+            this.btnFindNext.UseVisualStyleBackColor = true;
+            this.btnFindNext.Click += new System.EventHandler(this.btnFindNext_Click);
+            // 
+            // FindElementForm
+            // 
+            this.AcceptButton = this.btnFindNext;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(229, 70);
+            this.Controls.Add(this.btnFindNext);
+            this.Controls.Add(this.txtFindWhat);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FindElementForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Find";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FindElementForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFindWhat;
+        private System.Windows.Forms.Button btnFindNext;
+    }
+}
---
+++ VS2013/Source/UIMapToolbox.UI/FindElementForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UIMapToolbox.UI
+{
+    public partial class FindElementForm : Form
+    {
+        public FindElementForm()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler<FindNextEventArgs> FindNext;
+
+        private void btnFindNext_Click(object sender, EventArgs e)
+        {
+            if (FindNext != null)
+                FindNext(this, new FindNextEventArgs { Text = txtFindWhat.Text });
+        }
+
+        private void FindElementForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Hide();
+                e.Handled = true;
+            }
+        }
+
+        internal void SelectTextField()
+        {
+            this.txtFindWhat.Focus();
+            this.txtFindWhat.SelectAll();
+        }
+    }
+
+    public class FindNextEventArgs : EventArgs
+    {
+        public string Text { get; set; }
+    }
+}
